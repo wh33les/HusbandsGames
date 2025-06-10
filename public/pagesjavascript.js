7bd4fd9fafabe70ad62b52c6d2b8d30bb06d53b6
Ashley K. W. Warren <28959300+wh33les@users.noreply.github.com>
2025-06-10 13:14:28 -0600

    Copied some template files.

---
+++ public/pagesjavascript.js
@@ -0,0 +1,16 @@
+/* JavaScript used on all the main pages */
+
+$(function () {
+	$('#header').load('../pagesheader.html');
+}); /* Calls the header code */
+$(function () {
+	$('#lastmodified').load('../lastmodified.html');
+}); /* Calls the last modified code */
+function toggleShowHide(elementId) {
+	var element = document.getElementById(elementId);
+	if (element.style.display == "block") {
+		element.style.display = "none";
+	} else {
+		element.style.display = "block";
+	}
+} /* Toggles hidden text.  Code modified from https://www.toptip.ca/2009/09/showhide-text-on-web-page.html. */
